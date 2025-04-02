c3e13497bd9cfc3d64c668bf2b33009993165be9
qingshan754 <2041105585@qq.com>
2025-04-02 14:35:53 +0800

    更新了strcspn函数，移除了换行符的影响

--- 找到给定字符串的不同字符.c
+++ 找到给定字符串的不同字符.c
@@ -7,25 +7,32 @@ int main()
     char arr2[100];
     int hash1[256] = {0};
     int hash2[256] = {0};
-    int hash[256] = {0};
     fgets(arr1, sizeof(arr1), stdin);
+    arr1[strcspn(arr1, '\n')] = '\0'; // 移除了换行符
     fgets(arr2, sizeof(arr2), stdin);
+    arr2[strcspn(arr2, '\n')] = '\0'; // 移除了换行符
+
     for (int i = 0; i < strlen(arr1); i++)
     {
         hash1[(unsigned char)arr1[i]]++;
     }
+
     for (int i = 0; i < strlen(arr2); i++)
     {
         hash2[(unsigned char)arr2[i]]++;
     }
 
-    for (int i = 0; i < 256;i++){
-        if(hash1[i]>hash2[i]){
-            printf("%c",i);
+    for (int i = 0; i < 256; i++)
+    {
+        if (hash1[i] > hash2[i])
+        {
+            printf("%c", i);
         }
-        else if(hash1[i]<hash2[i]){
-            printf("%c",i);
+        else if (hash1[i] < hash2[i])
+        {
+            printf("%c", i);
         }
     }
-        return 0;
+
+    return 0;
 }
